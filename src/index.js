1fe370facb81db66729418b0c11d6587ab4494d1
Alyona Ihnatenko <altaig4@gmail.com>
2021-06-12 23:49:02 +0300

    end-hw-10

--- src/index.js
+++ src/index.js
@@ -8,4 +8,39 @@ const markupMenu= menuTpl(menu);
 menuRef.insertAdjacentHTML('beforeend',markupMenu);
 
 
+const toggle=document.querySelector('#theme-switch-toggle');
+ const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  }
 
+  toggle.addEventListener('change',handlerDarkTheme);
+
+  function handlerDarkTheme(event){
+    event.preventDefault();
+    if(localStorage.getItem('Theme')==='dark-theme'){
+         localStorage.setItem('Theme','light-theme');
+    
+     }
+     else{
+        localStorage.setItem('Theme','dark-theme');
+     }
+     addDarkTheme()
+    }
+
+     function addDarkTheme(){
+   
+           if(localStorage.getItem('Theme')==='dark-theme'){
+               document.querySelector('body').classList.add('dark-theme');
+               toggle.setAttribute('checked',true) ;
+
+            }
+         else{
+               document.querySelector('body').classList.remove('dark-theme');  
+           }
+        
+     }
+     addDarkTheme()
+   
+    
+     
