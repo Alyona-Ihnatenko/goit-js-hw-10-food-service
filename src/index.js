b3bbd6680568d08d46b9c9400fc744375895018d
Alyona Ihnatenko <altaig4@gmail.com>
2021-06-12 15:34:16 +0300

    make-tamplate

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import './sass/main.scss';
-import ingridients from "./menu.json";
-import images from "./images/sprite.svg";
+import menu from "./menu.json";
+import menuTpl from '../template/menuTpl.hbs';
+
+
+const menuRef = document.querySelector('.js-menu');
+const markupMenu= menuTpl(menu);
+menuRef.insertAdjacentHTML('beforeend',markupMenu);
+
+
 
